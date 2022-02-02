fa45a82c603eb7579ca3bc158649212418135375
Ali <ali.hounain@gmail.com>
2022-02-02 01:03:09 -0500

    Added command handler
    yep

--- main.js
+++ main.js
@@ -6,14 +6,37 @@ const client = new Discord.Client({
     // intents lets the discord bot know what to look out for
     intents: [
         "GUILDS", // guilds are discord servers essentially
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES", 
+        "GUILD_MEMBERS"
     ]
 });
+
+let bot = {
+    client, 
+    prefix: "n.",
+    owners: [344998078467670019]
+}
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
+
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
+client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
+
+client.loadEvents(bot, false)
+client.loadCommands(bot, false)
+
+
+// stores discord events 
+
+module.exports = bot
+
+/*
 // sets up the discord bot 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
 // logs to the console that the bot actually worked lol
+*/
 
 client.login(process.env.token)
 // logs into the application 
